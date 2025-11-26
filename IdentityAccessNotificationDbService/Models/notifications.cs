2271d3755e5cd77012b3337fe36b11033aff3403
Sadwale Shivaji <Sadwale.Shivaji@encora.com>
2025-11-26 14:39:40 +0530

    shivaji

---
+++ IdentityAccessNotificationDbService/Models/notifications.cs
@@ -0,0 +1,18 @@
+﻿namespace IdentityAccessNotificationDbService.Models
+{
+    public class notifications
+    {
+        public int id   { get; set; }
+        public int user_id { get; set; }
+        public string type { get; set; }
+        public NotificationChannel Channel { get; set; }
+    }
+    public enum NotificationChannel
+    {
+        Email,
+        Slack,
+        Webhook,
+        InApp
+    }
+
+}
---
+++ IdentityAccessNotificationDbService/Models/user_roles.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityAccessNotificationDbService.Models
+{
+    public class user_roles
+    {
+        public int user_id { get; set; }
+        public int role_id { get; set; }
+    }
+}
