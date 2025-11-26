386c7187f33a7e4922e8ff28bc402bce9abbdbc1
Sreenesh Reddy <hizahero@gmail.com>
2025-11-26 14:40:13 +0530

    sreenesh

---
+++ IdentityAccessNotificationDbService/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAccessNotificationDbService.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AuditLog> AuditLogs { get; set; }
+
+        public DbSet<Permission> Permissions { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<RolePermission> RolePermissions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<WorkFlow> WorkFlows { get; set; }
+
+    }
+}
---
+++ IdentityAccessNotificationDbService/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace IdentityAccessNotificationDbService.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public string DepartmentId{ get; set; }
+        public int ManagerId { get; set; }
+        
+        // 0 - inactive, 1 - active
+        public int Status { get; set; }
+
+        public DateTime CreatedAt { get; set; } 
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
